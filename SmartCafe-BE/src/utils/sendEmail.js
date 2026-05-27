21844b0c3bc8c6266c911694ca16f09cda28c346
Monil Babariya <monilbabariya1249@gmail.com>
2026-05-27 17:13:25 +0530

    Clean production email service logs

--- SmartCafe-BE/src/utils/sendEmail.js
+++ SmartCafe-BE/src/utils/sendEmail.js
@@ -1,33 +1,31 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendEmail = async (options) => {
-  try {
-    const client = SibApiV3Sdk.ApiClient.instance;
+  const client = SibApiV3Sdk.ApiClient.instance;
 
-    const apiKey = client.authentications["api-key"];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
+  const apiKey = client.authentications["api-key"];
 
-    const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+  apiKey.apiKey = process.env.BREVO_API_KEY;
 
-    await tranEmailApi.sendTransacEmail({
-      sender: {
-        email: "monilintech@gmail.com",
-        name: "SmartCafe",
+  const tranEmailApi =
+    new SibApiV3Sdk.TransactionalEmailsApi();
+
+  await tranEmailApi.sendTransacEmail({
+    sender: {
+      email: "monilintech@gmail.com",
+      name: "SmartCafe",
+    },
+
+    to: [
+      {
+        email: options.email,
       },
-      to: [
-        {
-          email: options.email,
-        },
-      ],
-      subject: options.subject,
-      textContent: options.message,
-    });
+    ],
 
-    console.log("EMAIL SENT");
-  } catch (error) {
-    console.log("EMAIL ERROR:", error);
-    throw error;
-  }
+    subject: options.subject,
+
+    textContent: options.message,
+  });
 };
 
 module.exports = sendEmail;
