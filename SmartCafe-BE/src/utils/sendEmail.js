6b2cccfedf296fff15401de899e2225e7332c8a0
Monil Babariya <monilbabariya1249@gmail.com>
2026-05-27 15:25:14 +0530

    Fix nodemailer SMTP config for production

--- SmartCafe-BE/src/utils/sendEmail.js
+++ SmartCafe-BE/src/utils/sendEmail.js
@@ -1,23 +1,33 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
