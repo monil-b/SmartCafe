3097b4e78371241092b14277853995da4c090116
Monil Babariya <monilbabariya1249@gmail.com>
2026-05-27 15:35:42 +0530

    Fix nodemailer smtp

--- SmartCafe-BE/src/utils/sendEmail.js
+++ SmartCafe-BE/src/utils/sendEmail.js
@@ -4,9 +4,9 @@ const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4, // force IPv4
+      port: 465,
+      secure: true,
+      family: 4,
 
       auth: {
         user: process.env.EMAIL_USER,
@@ -14,16 +14,14 @@ const sendEmail = async (options) => {
       },
     });
 
-    const mailOptions = {
+    await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: options.email,
       subject: options.subject,
       text: options.message,
-    };
+    });
 
-    await transporter.sendMail(mailOptions);
-
-    console.log("Email sent successfully");
+    console.log("EMAIL SENT");
   } catch (error) {
     console.log("EMAIL ERROR:", error);
     throw error;
